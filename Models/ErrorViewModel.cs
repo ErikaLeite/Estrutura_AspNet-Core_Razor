6ebd790545e626587416a14884eee99921aa495b
ERIKA LEITE PAIVA <you@example.com>
2021-01-12 10:36:03 -0300

    finalizado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Estrutura_AspNet_Core_Razor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
